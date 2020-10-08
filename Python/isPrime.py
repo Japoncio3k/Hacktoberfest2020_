c93a35d54baca0f58c0f136b6ba1983525387f96
Gustavo Seiji Kamihara <gustavo.kamihara@gmail.com>
2020-10-08 17:54:27 -0300

    Created a function that checks if a natural is prime

---
+++ Python/isPrime.py
@@ -0,0 +1,15 @@
+def isPrime(n):
+    if n == 0 or n == 1
+    if n == 2:
+        prime = True
+    else:
+        if n == 1 or n % 2 == 0:
+            prime = False
+        else:
+            prime = True
+            nat = 3                
+            while nat*nat <= n and prime:
+                if n % nat == 0:    
+                    prime = False
+                nat = nat + 2
+    return prime    
